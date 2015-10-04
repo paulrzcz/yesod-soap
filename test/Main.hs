75e0bd4ec241d475ba4bfbe7129c3437703d1944
Pavel <paul@paulrz.cz>
2015-10-04 19:43:20 +0200

    Custom namespace handing

--- test/Main.hs
+++ test/Main.hs
@@ -16,11 +16,30 @@ testSoapEnvelopeWithEncoding = "<s:Envelope xmlns:s=\"http://www.w3.org/2001/12/
 \  </s:Body>\n\
 \</s:Envelope>\n"
 
+testSoapEnvelopeWithRtcf = "<s:Envelope xmlns:s=\"http://www.w3.org/2001/12/soap-envelope\" xmlns:rtcf=\"http://rtcf.velrina.com\">\n\
+\  <s:Body>\n\
+\    <rtcf:id>Dummy body</rtcf:id>\n\
+\  </s:Body>\n\
+\</s:Envelope>\n"
 
 type StringEnvelope = SoapEnvelope IdBody
 
+type RtcfEnvelope = SoapEnvelope RtcfBody
+
+data RtcfBody = RtcfBody String
+    deriving (Show)
+
 data IdBody = IdBody String
     deriving (Show)
+
+xpElemRtcf = xpElemNS "http://rtcf.velrina.com" "rtcf"
+
+instance XmlPickler RtcfBody where
+    xpickle = xpElemRtcf "id" $
+        xpWrap (\s -> RtcfBody s,
+            \(RtcfBody s) -> s
+            ) $
+        xpText
 
 instance XmlPickler IdBody where
     xpickle = xpElem "id" $
@@ -49,10 +68,17 @@ deserializeTest2 = "deserializeTest with encodingStyle" ~: ( do
         Right x -> putStrLn $ show x
     )
 
+deserializeTest3 = "deserializeTest with custom namespace" ~: ( do
+    se <- (readEnvelope testSoapEnvelopeWithRtcf) :: IO (Either String RtcfEnvelope)
+    case se of
+        Left x -> assertString x
+        Right x -> putStrLn $ show x
+    )
+
 
 suite = TestSuite {
     suiteName = "Basic serialization",
-    suiteTests = [deserializeTest, serializeTest, deserializeTest2],
+    suiteTests = [deserializeTest, serializeTest, deserializeTest2, deserializeTest3],
     suiteConcurrently = False,
     suiteOptions = []
 }
